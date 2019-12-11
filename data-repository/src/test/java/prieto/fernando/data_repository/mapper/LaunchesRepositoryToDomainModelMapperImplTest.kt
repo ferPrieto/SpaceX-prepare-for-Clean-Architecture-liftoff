061d002f64021fc789a9e3538fa1118db0a30d60
fernando <fernando.moyano@nice.agency>
2019-12-11 19:13:05 +0000

    Added Parameterized tests (repository layer)

---
+++ data-repository/src/test/java/prieto/fernando/data_repository/mapper/LaunchesRepositoryToDomainModelMapperImplTest.kt
@@ -0,0 +1,105 @@
+package prieto.fernando.data_repository.mapper
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import prieto.fernando.data_repository.model.LaunchRepositoryModel
+import prieto.fernando.data_repository.model.LinksRepositoryModel
+import prieto.fernando.data_repository.model.RocketRepositoryModel
+import prieto.fernando.domain.model.LaunchDomainModel
+import prieto.fernando.domain.model.LinksDomainModel
+import prieto.fernando.domain.model.RocketDomainModel
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.test.assertEquals
+
+@RunWith(Parameterized::class)
+class LaunchesRepositoryToDomainModelMapperImplTest(
+    private val givenLaunches: List<LaunchRepositoryModel>,
+    private val expected: List<LaunchDomainModel>
+) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<List<Any>>> {
+            return listOf(
+                arrayOf(
+                    listOf(
+                        LaunchRepositoryModel(
+                            "missionName",
+                            buildDate("11-12-2019"),
+                            RocketRepositoryModel("rocketName", "rocketType"),
+                            LinksRepositoryModel("patchLink", "wikipediaLink", "videoLink"),
+                            false
+                        ),
+                        LaunchRepositoryModel(
+                            "missionName2",
+                            buildDate("11-12-2020"),
+                            RocketRepositoryModel("rocketName2", "rocketType2"),
+                            LinksRepositoryModel("patchLink2", "wikipediaLink2", "videoLink2"),
+                            false
+                        )
+                    )
+                    ,
+                    listOf(
+                        LaunchDomainModel(
+                            "missionName",
+                            buildDate("11-12-2019"),
+                            RocketDomainModel("rocketName", "rocketType"),
+                            LinksDomainModel("patchLink", "wikipediaLink", "videoLink"),
+                            false
+                        ),
+                        LaunchDomainModel(
+                            "missionName2",
+                            buildDate("11-12-2020"),
+                            RocketDomainModel("rocketName2", "rocketType2"),
+                            LinksDomainModel("patchLink2", "wikipediaLink2", "videoLink2"),
+                            false
+                        )
+                    )
+                ),
+                arrayOf(
+                    listOf(
+                        LaunchRepositoryModel(
+                            "missionName",
+                            buildDate("01-02-2010"),
+                            RocketRepositoryModel("rocketName", "rocketType"),
+                            LinksRepositoryModel("patchLink", "wikipediaLink", "videoLink"),
+                            true
+                        )
+                    ),
+                    listOf(
+                        LaunchDomainModel(
+                            "missionName",
+                            buildDate("01-02-2010"),
+                            RocketDomainModel("rocketName", "rocketType"),
+                            LinksDomainModel("patchLink", "wikipediaLink", "videoLink"),
+                            true
+                        )
+                    )
+                ),
+                arrayOf(emptyList(), emptyList())
+            )
+        }
+
+        private fun buildDate(dateValue: String) =
+            SimpleDateFormat("dd-MM-yyyy").parse(dateValue) ?: Date()
+    }
+
+    private lateinit var cut: LaunchesRepositoryToDomainModelMapperImpl
+
+    @Before
+    fun setUp() {
+        cut = LaunchesRepositoryToDomainModelMapperImpl()
+    }
+
+    @Test
+    fun `Given launchRepositoryModels when toDomainModel then expected result`() {
+        // When
+        val actualValue = cut.toDomainModel(givenLaunches)
+
+        // Then
+        assertEquals(expected, actualValue)
+    }
+}
---
+++ data-repository/src/test/java/prieto/fernando/data_repository/repository/SpaceXRepositoryImplTest.kt
@@ -0,0 +1,68 @@
+package prieto.fernando.data_repository.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockito_kotlin.*
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import prieto.fernando.data_repository.SpaceXRemoteSource
+import prieto.fernando.data_repository.mapper.CompanyInfoRepositoryToDomainModelMapper
+import prieto.fernando.data_repository.mapper.LaunchesRepositoryToDomainModelMapper
+
+@RunWith(MockitoJUnitRunner::class)
+class SpaceXRepositoryImplTest {
+    private lateinit var cut: SpaceXRepositoryImpl
+
+    @Mock
+    lateinit var spaceXRemoteSource: SpaceXRemoteSource
+
+    @Mock
+    lateinit var companyInfoDomainMapper: CompanyInfoRepositoryToDomainModelMapper
+
+    @Mock
+    lateinit var launchesDomainMapper: LaunchesRepositoryToDomainModelMapper
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        cut = SpaceXRepositoryImpl(spaceXRemoteSource, companyInfoDomainMapper, launchesDomainMapper)
+    }
+
+    @Test
+    fun `When getCompanyInfo then spaceXRemoteSource invoked`() {
+        // When
+        whenever(spaceXRemoteSource.getCompanyInfo())
+            .thenReturn(Single.just(mock()))
+
+        cut.getCompanyInfo()
+
+        // Then
+        val captor = ArgumentCaptor.forClass(Unit::class.java)
+        captor.run {
+            verify(spaceXRemoteSource, times(1)).getCompanyInfo()
+        }
+    }
+
+    @Test
+    fun `When getAllLaunches then spaceXRemoteSource invoked`() {
+        // When
+        whenever(spaceXRemoteSource.getCompanyInfo())
+            .thenReturn(Single.just(mock()))
+
+        cut.getCompanyInfo()
+
+        // Then
+        val captor = ArgumentCaptor.forClass(Unit::class.java)
+        captor.run {
+            verify(spaceXRemoteSource, times(1)).getCompanyInfo()
+        }
+    }
+}
