60642ce565a61c527afd1c1d4dc9e6a2f78a77ac
Fernando Prieto Moyano <fernandoprietomoyano@Fernandos-MacBook-Pro.local>
2021-12-04 12:38:24 +0000

    Abstracted dependencies objects

---
+++ buildSrc/src/main/kotlin/prieto/fernando/dependencies/ProjectModules.kt
@@ -0,0 +1,9 @@
+package prieto.fernando.dependencies
+
+object ProjectModules {
+    const val core = ":core"
+    const val api = ":data-api"
+    const val data = ":data"
+    const val domain = ":domain"
+    const val coreAndroidTest = ":core-android-test"
+}
