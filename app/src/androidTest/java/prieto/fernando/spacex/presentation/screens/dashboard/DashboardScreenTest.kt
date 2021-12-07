9d6acec85219cc2704833db51247eee6e9fcb354
Fernando Prieto Moyano <fernandoprietomoyano@Fernandos-MacBook-Pro.local>
2021-12-07 11:14:09 +0000

    Added first Dashboard UI Test

--- app/src/androidTest/java/prieto/fernando/spacex/presentation/screens/dashboard/DashboardScreenTest.kt
+++ app/src/androidTest/java/prieto/fernando/spacex/presentation/screens/dashboard/DashboardScreenTest.kt
@@ -1,19 +1,39 @@
 package prieto.fernando.spacex.presentation.screens.dashboard
 
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import prieto.fernando.spacex.presentation.EntryPointActivity
+import prieto.fernando.spacex.presentation.screens.MainScreen
+import prieto.fernando.spacex.theme.SpaceXTheme
 
+@ExperimentalMaterialApi
 @RunWith(AndroidJUnit4::class)
 class DashboardScreenTest  {
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<EntryPointActivity>()
+
+    @InternalCoroutinesApi
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            SpaceXTheme {
+                MainScreen()
+            }
+        }
+    }
 
     @Test
-    fun postsContainError_snackbarShown() {
-
+    fun clickBottomNavigationItemToLaunchesScreen() {
+        composeTestRule.onNodeWithText("Launches", useUnmergedTree = true).performClick().assertIsDisplayed()
+        composeTestRule.onNodeWithText("LAUNCHES", useUnmergedTree = true).assertIsDisplayed()
     }
 }
