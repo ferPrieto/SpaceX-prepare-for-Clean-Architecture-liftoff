d796a89063e8b6fd3d5974cde9cb8e93314928fc
Fernando Prieto Moyano <fernandoprietomoyano@Fernandos-MacBook-Pro-2.local>
2022-02-09 21:49:16 +0000

    Added new gradlew distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
     }
 }
