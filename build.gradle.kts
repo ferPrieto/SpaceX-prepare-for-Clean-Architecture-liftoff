26dd9886c7f0ebac5910c84d04b3bfe85090b75b
ferPrieto <fprieto@godaddy.com>
2022-12-08 19:32:30 +0000

    Updated Code Coverage  [skip ci]
    some descr [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
-
         classpath("com.karumi:shot:5.13.0")
     }
 }
