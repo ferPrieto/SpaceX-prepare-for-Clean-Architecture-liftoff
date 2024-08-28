ef6b378a00fa823c8f3e3639cc59c53b12692d7a
ferPrieto <fprieto@godaddy.com>
2024-08-28 22:33:59 +0100

    Updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         classpath("com.karumi:shot:5.13.0")
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.android.tools.build:gradle:7.3.1")
+    implementation("com.android.tools.build:gradle:7.4.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
 
     implementation(gradleApi())
